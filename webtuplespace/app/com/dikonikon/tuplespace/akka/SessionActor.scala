8c34b7e3b68738c880c925cad73059dcdad14aaf
dikonikon <dikonikon@gmail.com>
2013-09-29 00:01:30 +0100

    started on state model for subs sessions

--- webtuplespace/app/com/dikonikon/tuplespace/akka/SessionActor.scala
+++ webtuplespace/app/com/dikonikon/tuplespace/akka/SessionActor.scala
@@ -13,6 +13,8 @@ package com.dikonikon.tuplespace.akka
 import akka.actor.Actor
 import play.api.libs.iteratee.Concurrent.Channel
 import play.api.Logger
+import com.dikonikon.tuplespace.WebTuple
+import scala.xml.{ NodeSeq }
 
 class SessionInActor extends Actor {
   def receive = {
@@ -38,6 +40,53 @@ class SimpleOutActor[T] extends Actor {
     case s: T => {
       Logger.debug("actor received: " + s)
       channel.push(s)
+    }
+  }
+}
+
+class SubscriptionActor[T] extends Actor {
+  var channel: Channel[T] = null
+  val me = "SubscriptionActor: "
+  var pattern: WebTuple = null
+
+  def receive = expectChannel orElse unexpected
+
+  def expectChannel: Receive = {
+    case c: Channel[T] => {
+      Logger.debug(me + "received Channel")
+      context.become(expectSubscription orElse unexpected)
+    }
+  }
+
+  def expectSubscription: Receive = {
+    case <subscription>{content}</subscription> => {
+      sendCurrentStateStart
+      sendCurrentStateMatches(content)
+      sendEndCurrentState
+      context.become(expectNewTuple orElse unexpected)
+    }
+  }
+
+  def sendCurrentStateStart: Unit = {
+
+  }
+
+  def sendCurrentStateMatches(pattern: NodeSeq): Unit = {
+
+  }
+
+  def sendEndCurrentState: Unit = {
+
+  }
+
+  def expectNewTuple: Receive = {
+
+  }
+
+  def unexpected: Receive = {
+    case x => {
+      Logger.error(me + "received: " + x + " but don't know what to do with it")
+      Logger.error(me + "type of message received was: " + x.getClass.toString)
     }
   }
 }
